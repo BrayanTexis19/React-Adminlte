a002e2561f7adb49e812335ef7b2d815af569f46
test1 <utp0145349@alumno.utpuebla.edu.mx>
2023-06-02 11:27:51 -0600

    componente Datatable agregado

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Header from './components/Header';
 import Aside from './components/Aside';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import Datatable from './components/Datatable';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Aside />
-      <Content />
-      <Footer />
+      <Datatable/>
     </>
   )
 }
