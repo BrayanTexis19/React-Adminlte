30afe89c60ae3631248f403c79cbbcbb1ddbf635
test1 <utp0145349@alumno.utpuebla.edu.mx>
2023-06-02 10:55:35 -0600

    componente de logeo agregado

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import Header from './components/Header';
 import Aside from './components/Aside';
 import Content from './components/Content';
 import Footer from './components/Footer';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Aside />
-      <Content />
-      <Footer />
+        <Login/>
     </>
   )
 }
